8db16624dc22c38a0460566a278ca73f4cb36ffa
Marco de Castro <castromar189@gmail.com>
2024-04-21 22:17:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import nodemon from 'nodemon';
+
+const app = express();
+
+
+// Cria uma rota para teste
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+);
+
+// Inicia o servidor
+app.listen(5000, () => {
+    console.log('Server started on http://localhost:5000');
+}
+);
