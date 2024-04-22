ef539669020d9e0379e555cb836e60d363da1a5a
Marco de Castro <castromar189@gmail.com>
2024-04-22 16:42:59 -0300

    model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { body, validationResult } from 'express-validator';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import User from './models/User.js';
 
 dotenv.config();
 
