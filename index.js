25fcb96640aa481359e4256771819237e0bc3534
Marco de Castro <castromar189@gmail.com>
2024-04-22 21:00:48 -0300

    mongoose

--- index.js
+++ index.js
@@ -19,7 +19,29 @@ const corsOptions = {
 };
 
 
-const PORT = process.env.PORT  
+const PORT = process.env.PORT 
+ 
+
+// Connect to MongoDB por string
+const mongoDBUrl = process.env.MONGODB_URI 
+
+if (!mongoDBUrl) {
+  throw new Error('MONGODB_URI environment variable is not defined');
+}
+
+mongoose.connect(mongoDBUrl, {
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error('Erro ao conectar ao MongoDB:', error.message);
+  process.exit(1);
+});
+db.once('open', () => {
+  console.log('Conexão com o MongoDB estabelecida com sucesso');
+});
+
+
 
 
 
